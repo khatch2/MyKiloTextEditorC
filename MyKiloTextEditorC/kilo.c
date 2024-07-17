339b4b122294a1101e1f2f1adb9fb54dcc576373
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-07-17 15:25:49 +0200

    Window size, the easy way

--- MyKiloTextEditorC/kilo.c
+++ MyKiloTextEditorC/kilo.c
@@ -7,6 +7,7 @@ khatchik
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -17,6 +18,8 @@ khatchik
 struct termios orig_termios;
 
 struct editorConfig {
+    int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -62,10 +65,21 @@ char editorReadKey() {
   return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output ***/
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -92,9 +106,15 @@ void editorProcessKeypress() {
 
 
 /*** init ***/
+
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
 
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
